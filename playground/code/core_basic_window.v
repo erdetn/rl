335cbf7c4b856ff370ef7f305b6524de59df9b6e
Erdet Nasufi <erdet.nasufi@gmail.com>
2022-07-28 21:29:30 +0200

    providing first 3d world using rl in v

--- playground/code/core_basic_window.v
+++ playground/code/core_basic_window.v
@@ -6,6 +6,7 @@ import rl.color
 import rl.text
 import rl.draw
 import rl.geometry as rg
+import rl.camera as cam 
 
 const screen_width  = 800
 const screen_height = 450 
@@ -13,9 +14,12 @@ const screen_height = 450
 fn main() {
     window.init(screen_width, screen_height, 'raylib [core] example - basic window')
 
-    cylinder := draw.new_cylinder(rg.Vector3{0, 0, 0}, f32(10), f32(20), f32(100), 10, color.Color{100, 100, 0, 255})
+    cam3d := cam.new(rg.Vector3{0, 10, 10}, rg.Vector3{0, 0, 0}, rg.Vector3{0, 1, 0}, f32(120), cam.CameraProjection.perspective)
+    cam3d.set_mode(cam.CameraMode.free)
+    
+    cylinder := draw.new_cylinder(rg.Vector3{0, 0, 0}, f32(2), f32(2), f32(2), 10, color.Color{200, 180, 0, 250})
     plane := draw.new_plane(rg.Vector3{10, 10, 0}, rg.Vector2{40, 40}, color.Color {200, 200, 200, 255})
-    grid := draw.new_grid(20, f32(20))
+    grid := draw.new_grid(10, f32(1))
     line := draw.new_line(rg.Vector2{50, 50}, rg.Vector2{250, 250}, f32(3.0), color.Color{255, 10, 10, 255})
 
     pxl := draw.new_pixel(100, 100, color.Color{255, 0, 0, 255})
@@ -28,17 +32,25 @@ fn main() {
            break
         }
 
+        cam3d.update()
+
         rl.begin_drawing()
 
-        rl.clear_background(color.gray) 
+        rl.clear_background(color.Color{50, 50, 50, 255}) 
+        
+        cam3d.begin()
         
         plane.draw()
+        cylinder.draw()
+        //C.DrawCube(C.Vector3{0, 0, 0}, f32(10), f32(10), f32(10), C.Color{255, 0,0, 255})
+        grid.draw()
+
+        cam3d.end()
+
         txt_fps.draw()
         txt.draw()
         pxl.draw()
         line.draw()
-        grid.draw()
-        cylinder.draw()
 
         rl.end_drawing()
 
